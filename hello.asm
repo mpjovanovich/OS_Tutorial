0a039a1a4a4f1da8a92fde44a559af83e5126f41
mpjovanovich <mpjovanovich@gmail.com>
2021-05-16 16:42:39 -0500

    Finished hello basic.

--- hello.asm
+++
@@ -1,6 +0,0 @@
-;
-; A simple boot sector that prints a message to the screen
-; using a BIOS routine.
-;
-
-
---
+++ hello_basic.asm
@@ -0,0 +1,32 @@
+;
+; A simple boot sector that prints a message to the screen
+; using a BIOS routine.
+;
+
+; We want interrupt 10, sub code ah.
+; (int 10, ah = 0eh) = scrolling teletype BIOS routine
+
+; Set the high order byte for register ax as part of the 
+; instruction for scrolling teletype.
+mov ah, 0x0e
+
+; Set the low order byte to the character to be printed,
+; Then send the interrupt to the "screen" interrupt table
+; address (0x10).
+mov al, 'H'
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'o'
+int 0x10
+
+; Jump th the ucrrent address (infinitely).
+jmp $
+
+; Padding and magic BIOS number for boot sector.
+times 510-($-$$) db 0
+dw 0xaa55
